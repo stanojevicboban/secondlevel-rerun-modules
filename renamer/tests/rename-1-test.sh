fa5a1f6aadd1abd1e0bc74b5412603ec8c543665
andremolnar <andre@becircle.com>
2013-02-05 12:06:47 -0500

    Adding in a renamer rerun script which renames *skeletor*.* to *whatever*.*

---
+++ renamer/tests/rename-1-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env roundup
+#
+#/ usage:  rerun stubbs:test -m renamer -p rename [--answers <>]
+#
+
+# Helpers
+# -------
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+describe "rename"
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
